9ff66a3e259abf0f8d18d1e852f83be6480ec10c
Sebastian Christiansen <32192177+Sebastianchr22@users.noreply.github.com>
2018-10-24 13:52:51 +0200

    added doc

--- 3_semester/src/SecuritySystem/Hasher.java
+++ 3_semester/src/SecuritySystem/Hasher.java
@@ -10,18 +10,33 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 /**
+ * Hasher class used for hashing a string value
  *
  * @author Sebas
  */
 public class Hasher {
+
+    /**
+     * Hashes the value from a string to a byte array, then calls bytesToHex()
+     *
+     * @param value : String
+     * @return Hashed value
+     * @throws NoSuchAlgorithmException
+     */
     public String hash(String value) throws NoSuchAlgorithmException {
         MessageDigest digest = MessageDigest.getInstance("SHA-256");
         byte[] encodedhash = digest.digest(
                 value.getBytes(StandardCharsets.UTF_8));
-        
+
         return bytesToHex(encodedhash);
     }
 
+    /**
+     * Converts a byte array to a hex string
+     *
+     * @param hash : byte array
+     * @return A convertion of a byte array to a hex string
+     */
     private static String bytesToHex(byte[] hash) {
         StringBuffer hexString = new StringBuffer();
         for (int i = 0; i < hash.length; i++) {
