37cd022c7f4d201a5b038373c6268e887cc43618
Ahmad Hamid <Ahham14@student.sdu.dk>
2018-10-30 13:44:06 +0100

    Revert "Revert "ClientDomainEdit""
    This reverts commit fad4408448136b52f5c3cc39aa610925760898ef.

--- 3_semester_ServerPersistence/src/pkg3_semester_serverpersistence/QueryHandler.java
+++ 3_semester_ServerPersistence/src/pkg3_semester_serverpersistence/QueryHandler.java
@@ -45,7 +45,7 @@ public class QueryHandler implements IQueryHandler {
     @Override
     public ResultSet getQuestionSet() throws SQLException {
         Connection con = connect();
-        PreparedStatement statement = con.prepareStatement("SELECT * FROM \"P");
+        PreparedStatement statement = con.prepareStatement("SELECT * FROM PQuestionSet");
         
         return statement.executeQuery();
        
