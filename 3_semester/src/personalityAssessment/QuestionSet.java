649e2c235f4f0e7346cfa1a62212aaf814a3fbd7
Ahmad Hamid <Ahham14@student.sdu.dk>
2018-10-30 11:03:45 +0100

    Client Question + QuestionSet update

--- 3_semester/src/personalityAssessment/QuestionSet.java
+++ 3_semester/src/personalityAssessment/QuestionSet.java
@@ -5,8 +5,11 @@
  */
 package personalityAssessment;
 
+import ProjectInterfaces.IQuestion;
 import ProjectInterfaces.IQuestionSet;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -15,12 +18,41 @@ import java.io.Serializable;
 public class QuestionSet implements IQuestionSet, Serializable {
 
     private PersonalityFacade pf;
-    private Question q1;
+    private List<IQuestion> questions = new ArrayList();
 
     public QuestionSet(PersonalityFacade pf) {
-        this.pf = pf;
+        this.pf = pf; 
+    }
 
+    public QuestionSet(List<Question> ql) {
+        int i = 0;
+        while (!(ql.size() == (i + 1))) {            
+            addToList(new Question(ql.get(i)));
+            i++;
+        }
+    }
+    
+    
+
+    @Override
+    public List<IQuestion> getQuestionSet() {
+        return questions;
+    }
+
+    @Override
+    public void addToList(IQuestion q) {
+        questions.add(q);
+    }
+
+    @Override
+    public List<Integer> getAnswers() {
+        List<Integer> l = new ArrayList<>();
+        int i = 0;
         
+        while (!(questions.size() == i + 1)) {
+            l.add(questions.get(i).getQuestionAnswer());
+        }
+        return l;
     }
 
     
