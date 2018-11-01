af5a59862a7a562b4264935027c50fe32bbe829d
muggell <32263698+muggell@users.noreply.github.com>
2018-11-01 17:10:19 +0100

    Fixed SQL

--- 3_semester_ServerPersistence/src/pkg3_semester_serverpersistence/QueryHandler.java
+++ 3_semester_ServerPersistence/src/pkg3_semester_serverpersistence/QueryHandler.java
@@ -67,33 +67,16 @@ public class QueryHandler implements IQueryHandler {
     @Override
     public void applyForJob(IJobPost jobpost, IUser applicant) throws SQLException {
         Connection con = connect();
-        PreparedStatement statement;
+        PreparedStatement statement, statement2;
+        
+        String job = "Job" + jobpost.getId() + "_applicants";
+        String jobTable = "job" + jobpost.getId() + "_user_id";
 
-        if (jobpost.getId() == 1) {
-            statement = con.prepareStatement("INSERT INTO \"Job_1\" WHERE \"Job_1\".job1_user_id = ?");
-            statement.setInt(1, applicant.getUserId());
-            statement.executeQuery();
-
-        } else if (jobpost.getId() == 2) {
-            statement = con.prepareStatement("INSERT INTO \"Job_2\" WHERE \"Job_2\".job2_user_id = ?");
-            statement.setInt(1, applicant.getUserId());
-            statement.executeQuery();
-
-        } else if (jobpost.getId() == 3) {
-            statement = con.prepareStatement("INSERT INTO \"Job_3\" WHERE \"Job_3\".job3_user_id = ?");
-            statement.setInt(1, applicant.getUserId());
-            statement.executeQuery();
-
-        } else if (jobpost.getId() == 4) {
-            statement = con.prepareStatement("INSERT INTO \"Job_4\" WHERE \"Job_4\".job4_user_id = ?");
-            statement.setInt(1, applicant.getUserId());
-            statement.executeQuery();
-
-        } else if (jobpost.getId() == 5) {
-            statement = con.prepareStatement("INSERT INTO \"Job_5\" WHERE \"Job_5\".job5_user_id = ?");
-            statement.setInt(1, applicant.getUserId());
-            statement.executeQuery();
-        }
+        statement = con.prepareStatement("INSERT INTO" + job + " WHERE " + jobTable + " = ?");
+        statement.setInt(1, applicant.getUserId());
+        statement.executeQuery();
+        
+        statement2 = con.prepareStatement("SELECT * FROM job1_applicants");
 
     }
 
@@ -101,7 +84,7 @@ public class QueryHandler implements IQueryHandler {
     public ResultSet getApplicants() throws SQLException {
         Connection con = connect();
         PreparedStatement statement = con.prepareStatement("SELECT * FROM \"Job_1\" UNION SELECT * FROM \"Job_2\" UNION SELECT * FROM \"Job_3\" UNION SELECT * FROM \"Job_4\" UNION SELECT * FROM \"Job_5\"");
-        
+
         return statement.executeQuery();
     }
 
